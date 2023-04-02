c6271d204b3f6365852eb81c649c7573143c8ab3
Reynaldi <reynaldiwijaya2306@gmail.com>
2023-04-03 01:19:59 +0700

    Add initialization of `LoginViewController` and parsing argument that needed.

--- SimpleLogin/Login/Presentation/LoginViewController.swift
+++ SimpleLogin/Login/Presentation/LoginViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 final class LoginViewController: UIViewController {
     
     private let loginView: LoginView = .init(frame: .zero)
+    private let loginService: LoginService
+    
+    init(loginService: LoginService) {
+        self.loginService = loginService
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
